91abda1ad3db9deb8d9fbb68b400f1b3c1c44427
Pannawich Khongtoem <pannawich250@gmail.com>
2023-03-12 21:21:21 +0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createClient} from '@supabase/supabase-js';
+import {SessionContextProvider} from '@supabase/auth-helpers-react';
+
+const supabase = createClient(
+  "https://zmcqffuidpwphwiweczq.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptY3FmZnVpZHB3cGh3aXdlY3pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg2MjgxMDksImV4cCI6MTk5NDIwNDEwOX0.BMo3WEVY6pCDXr4f3sQOs5U1UJtl-behAYbJJdvkNM4"
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
   </React.StrictMode>
 );
 
